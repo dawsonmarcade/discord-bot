ec9aa0af5309e0fed67a70c9fce8e646dd4ee461
Dawson <dawsonmarcade@gmail.com>
2025-11-18 13:24:55 -0600

    Working on register commands for /, key still not working within the file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 require('dotenv').config;
 const { Client, IntentsBitField } = require('discord.js');
+const {SlashCommandBuilder} =require('discord.js');
+
+
 
 const client = new Client({
     intents: [
@@ -21,6 +24,8 @@ client.on('messageCreate', (message) => {
 
 });
 
-//client.login(process.env.TOKEN);
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+})
 
 console.log(process.env.TOKEN);
