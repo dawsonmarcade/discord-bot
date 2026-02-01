e64c1f65e22c04e9f35b7e839100ad87015ac7e0
Dawson <dawsonmarcade@gmail.com>
2026-02-01 16:25:15 -0600

    Continue building out scraper.py looking for specific image

--- scraper.py
+++ scraper.py
@@ -1,8 +1,3 @@
-# import os
-# import requests
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 
 import bs4
 
@@ -17,6 +12,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
+image_class = "eA0Zlc PZPZlf WghbWd FnEtTd mkpRId m3LIae RLdvSe qyKxnc ivg-i GMCzAd"
 
 search_url = "https://www.google.com/search?q=Lebron James&source=lnms&tbm=isch"
 driver.get(search_url)
@@ -27,7 +23,13 @@ driver.execute_script("window.scrollTo(0,0)")
 
 page_html = driver.page_source
 pageSoup = bs4.BeautifulSoup(page_html, 'html.parser')
-containers = pageSoup.find_all('div', {'class':"PG8i1e 0oz8t PyLEff"})
+containers = pageSoup.find_all('div', {'class':image_class})
 
 len_containers = len(containers)
-print(f"Found {len_containers} containers")
+
+
+print("the number of containers is: ", len_containers)
+
+# driver.find_element('//*[@id="rso"]/div/div/div[1]/div/div/div[1]',by="xpath").click()   
+
+driver.find_element(by='xpath',value= '//*[@id="rso"]/div/div/div[1]/div/div/div[1]').click() 
